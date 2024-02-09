04d04570f156948c33eb538e147338c134740261
NikVAx <nik.vasilenko1203@gmail.com>
2024-02-09 12:27:25 +0300

    Add basic login form

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,9 +1,35 @@
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import s from "./loginPage.module.css";
+import { Button, Paper } from "@mui/material";
 
 export function LoginPage() {
   return (
-    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-      Login Page Content
-    </Typography>
+    <div className={s.container}>
+      <div className={s.bgImage}></div>
+      <div className={s.formContainer}>
+        <Paper elevation={3} className={s.formContent}>
+          <Typography>
+            Login
+          </Typography>
+
+          <TextField      
+            id="outlined-required"
+            label="Username"
+            defaultValue=""
+            autoComplete="off"
+          />
+          <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="new-password"
+          />
+          <Button variant="contained">
+              Login
+          </Button>
+        </Paper>
+      </div>
+    </div>
   );
 }
